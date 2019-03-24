24c9e65653cdcc65e53c487e4fee3617401f4680
Gomathi799 <46838489+Gomathi799@users.noreply.github.com>
2019-03-24 17:00:00 +0530

    158.c

---
+++ 158.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c,r,n=0,i=1,k,l=0;
+    scanf("%d%d",&a,&b);
+    c=a*b;
+    while(c)
+    {
+        r=c%2;
+        n=n+(i*r);
+        i=i*10;
+        c=c/2;
+    }
+    printf("%d ",n);
+    while(n)
+    {
+        k=n%10;
+        l++;
+        if(k==1)
+        {
+            printf("%d",l+1);
+            break;
+        }
+        else
+        n=n/10;
+    }
+}
