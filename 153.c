9fc42936fe18843e5f7bdf4512737fbb4da4e8c4
Gomathi799 <46838489+Gomathi799@users.noreply.github.com>
2019-03-24 16:27:31 +0530

    153.c

---
+++ 153.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    char a[1000];
+    int n,k=0,i,j;
+    scanf("%s",a);
+    scanf("%d",&k);
+    for(i=0;a[i]!='\0';i++);
+    n=i;
+    for(i=0;i<n;i++)
+    {
+        if((i+1)%k==0)
+        {
+            printf("%c ",a[i]);
+        }
+    }
+}
